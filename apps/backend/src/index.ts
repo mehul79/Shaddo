a83699ed227dd1047f31d02d27776b38a391f8da
Mehul Gupta <mehulrgupta@gmail.com>
2025-11-01 16:28:30 +0530

    be muted

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,7 +1,7 @@
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import jwt from 'jsonwebtoken';
-import prisma from '@repo/db/src/main';
+// import prisma from '@repo/db/src/main';
 
 const app = express();
 
@@ -9,16 +9,16 @@ app.get("/", (req:Request,res:Response)=>{
     res.send("Hello from backend");
 })
 
-app.get("/hi", async(req:Request, res:Response)=>{
-    res.send("Hi from backend");
-    const result = await prisma.user.create({
-        data:{
-            email: "mehul@example.com",
-            name: "Mehul"
-        }
-    })
-    console.log(result);
-})
+// app.get("/hi", async(req:Request, res:Response)=>{
+//     res.send("Hi from backend");
+//     const result = await prisma.user.create({
+//         data:{
+//             email: "mehul@example.com",
+//             name: "Mehul"
+//         }
+//     })
+//     console.log(result);
+// })
 
 app.listen(4000, ()=>{
     console.log("Server started on port 4000");
